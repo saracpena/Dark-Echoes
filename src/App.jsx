dc81a9ea1ffbfd6ae80e56515fa285599194bceb
Sara <s.cristal.p@gmail.com>
2026-06-29 19:41:40 +0200

    npm package and vars

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+import { data } from "./data.js";
+
 export default function App() {
-  // TODO
+  const [episodeList, setEpisodeList] = useState(data);
+  const [oneEpisodeId, setEpisodeId] = useState = useState(null);
+  const selectedEpisodeId = episodeList.find((episode) => {
+    return episode.id === selectedEpisodeId;
+  });
+
+  
 }
