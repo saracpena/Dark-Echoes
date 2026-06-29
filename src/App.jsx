f4a9e074488ed429c4d53187ce7a8873b0445caf
Sara <s.cristal.p@gmail.com>
2026-06-29 20:35:44 +0200

    many clairifying notes for me

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,45 @@
 import { useState } from "react";
 import "./App.css";
-import { data } from "./data.js";
+import { data, episodeList } from "./data";
 
 export default function App() {
-  const [episodeList, setEpisodeList] = useState(data);
-  const [oneEpisodeId, setEpisodeId] = useState = useState(null);
-  const selectedEpisodeId = episodeList.find((episode) => {
+  // State or in plain english "REACT'S MEMORY"
+  /** 
+   * episodes = the current value React is remembering
+     setEpisodes = the function used to change that list
+     episodeList = the starting data from data.js
+So therefore,
+     const [episodes, setEpisodes] = useState(episodeList); 
+     
+  //* React asks:
+
+  * ?What data should I keep track of?
+
+    *!You answer: Episodes.
+
+  // * React asks:
+
+*   ?What should they be when the app first starts?
+
+  *!You answer: The array from data.js which is "episodeList"
+
+  //* So React internally remembers
+
+    episodes
+
+      ↓
+
+    episodeList
+    
+    *! React only cares about the initial value/state */
+  
+  const [episodes, setEpisodes] = useState(episodeList);
+  const [selectedEpisodeId, setselectedEpisodeId] = useState(null);
+  // Derived data
+  const selectedEpisode = episodeList.find((episode) => {
     return episode.id === selectedEpisodeId;
   });
 
-  
+
+
 }
